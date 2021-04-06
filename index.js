08e8ff6614c98e9f3ea48177b4880c689b4b309a
Diana Liao <dijali@gmail.com>
2021-04-05 20:56:03 -0400

    commit tree 0b7991a9bd37e88058e1f1ddc3f901ed142c78ca

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let mainMain = document.querySelector('main#main')
+mainMain.remove()
+
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Sqkrz is the champion'
+
+
